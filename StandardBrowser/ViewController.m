00ab4bb22a184bd07c8bc032bfef68377cc60dcd
Binsent7 <hishinuma.kougakuin@gmail.com>
2015-11-23 19:03:55 +0900

    コメントを追加

--- StandardBrowser/ViewController.m
+++ StandardBrowser/ViewController.m
@@ -11,101 +11,65 @@
 @interface ViewController ()
 
 // storyboard上のUIパーツとコードの紐付け
-@property (weak, nonatomic) IBOutlet UIWebView *webView;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *browserBackButton;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *browserForwardButton;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *stopButton;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;                    // WebView
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *browserBackButton;    // 戻るボタン
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *browserForwardButton; // 進むボタン
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *stopButton;           // 読み込み中止ボタン
 
 // storyboard上のUIパーツのイベントとコードの紐付け
-- (IBAction)onTapBrowserBackButton:(id)sender;
-- (IBAction)onTapBrowserForwardButton:(id)sender;
-- (IBAction)onTapRefreshButton:(id)sender;
-- (IBAction)onTapStopButton:(id)sender;
+- (IBAction)onTapBrowserBackButton:(id)sender;      // 戻るボタンタップイベント
+- (IBAction)onTapBrowserForwardButton:(id)sender;   // 進むボタンタップイベント
+- (IBAction)onTapRefreshButton:(id)sender;          // 再読込ボタンタップイベント
+- (IBAction)onTapStopButton:(id)sender;             // 読み込み中止ボタンタップイベント
 
 @end
 
 @implementation ViewController
 
-#pragma mark -- Lifecycle
+// pragma markについて
+// http://zutto-megane.com/objective-c/post-404/
+#pragma mark - Lifecycle
 
 /*
- ロード時にコール
+ ロード時にコールされる
  ※1度しか呼ばれない
  */
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
+    // webViewイベントが通知されるようdelegateを設定
     self.webView.delegate = self;
     
-    // リクエスト
-    NSString *urlString     = @"http://google.co.jp";
-    NSURL *requestUrl       = [NSURL URLWithString:urlString];
-    [self loadWebViewRequestWithUrl:requestUrl];
+    // リクエスト作成
+    NSString *requestURLString  = @"http://google.co.jp";
+    NSURL *requestUrl           = [NSURL URLWithString:requestURLString];
+    
+    // webViewページを作成したリクエストでロード
+    [self loadWebViewRequestWithURL:requestUrl];
 }
 
 /*
- 画面表示直前にコール
+ 画面表示直前にコールされる
+ 画面を切り替える度に呼ばれる
  */
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
     
+    // ツールバー内に配置されているボタン表示状態を更新
     [self updateToolBarButtons];
 }
 
+/*
+ メモリ警告時にコールされる
+ */
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
 }
 
-#pragma mark -- Private
-
-/*
- 引数のURLを用いてWebページのロードを開始する
- @param url :リクエストするURL
- */
-- (void)loadWebViewRequestWithUrl:(NSURL *)url
-{
-    NSURLRequest *request   = [NSURLRequest requestWithURL:url];
-    [self.webView loadRequest:request];
-}
-
-/*
- ツールバー内に配置されている各ボタンの状態を更新する
- */
-- (void)updateToolBarButtons
-{
-    // 戻るボタン
-    if ([self.webView canGoBack]) {
-        // 一つ前のページへの遷移：可能
-        self.browserBackButton.enabled = YES;
-    }
-    else {
-        // 一つ前のページへの遷移：不可能
-        self.browserBackButton.enabled = NO;
-    }
-    
-    // 進むボタン
-    if ([self.webView canGoForward]) {
-        // 一つ後のページへの遷移：可能
-        self.browserForwardButton.enabled = YES;
-    }
-    else {
-        // 一つ後のページへの遷移：不可能
-        self.browserForwardButton.enabled = NO;
-    }
-    
-    // 中止ボタン
-    if ([self.webView isLoading]) {
-        self.stopButton.enabled = YES;
-    }
-    else {
-        self.stopButton.enabled = NO;
-    }
-}
-
-#pragma mark -- <UIWebViewDelegate>
+#pragma mark - <UIWebViewDelegate>
 
 /*
  ロード時にコール
@@ -116,7 +80,9 @@
  */
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
 {
+    // ツールバー内に配置されているボタン表示状態を更新
     [self updateToolBarButtons];
+    
     return YES;
 }
 
@@ -126,8 +92,10 @@
  */
 - (void)webViewDidStartLoad:(UIWebView *)webView
 {
+    // ツールバー内に配置されているボタン表示状態を更新
     [self updateToolBarButtons];
     
+    // ステータスバーにインジケータを表示する
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
 }
 
@@ -137,7 +105,10 @@
  */
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
+    // ツールバー内に配置されているボタン表示状態を更新
     [self updateToolBarButtons];
+    
+    // ステータスバーのインジケータを非表示にする
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
 }
 
@@ -148,18 +119,23 @@
  */
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
 {
+    // ツールバー内に配置されているボタン表示状態を更新
     [self updateToolBarButtons];
+    
+    // ステータスバーのインジケータを非表示にする
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
 }
 
-#pragma mark -- Button Action Method
+#pragma mark - Button Action Method
 
 /*
  戻るボタンタップ時にコール
  */
 - (IBAction)onTapBrowserBackButton:(id)sender
 {
+    // webViewでページが戻れるかで判別
     if ([self.webView canGoBack]) {
+        // webViewのページを一つ戻る
         [self.webView goBack];
     }
 }
@@ -169,7 +145,9 @@
  */
 - (IBAction)onTapBrowserForwardButton:(id)sender
 {
+    // webViewでページが進めるかで判別
     if ([self.webView canGoForward]) {
+        // webViewのページを一つ進める
         [self.webView goForward];
     }
 }
@@ -179,6 +157,7 @@
  */
 - (IBAction)onTapRefreshButton:(id)sender
 {
+    // webViewで現在表示されているページを再読み込み
     [self.webView reload];
 }
 
@@ -187,8 +166,58 @@
  */
 - (IBAction)onTapStopButton:(id)sender
 {
+    // webViewが読み込み中かで判別
     if ([self.webView isLoading]) {
+        // webViewの読み込みを中止
         [self.webView stopLoading];
+    }
+}
+
+#pragma mark - Private
+
+/*
+ 引数のURLを用いてWebページのロードを開始する
+ @param url :リクエストするURL
+ */
+- (void)loadWebViewRequestWithURL:(NSURL *)url
+{
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
+
+/*
+ ツールバー内に配置されている各ボタンの状態を更新する
+ */
+- (void)updateToolBarButtons
+{
+    // 戻るボタン表示状態更新(webViewでページが戻れるかで判別)
+    if ([self.webView canGoBack]) {
+        // 一つ前のページへの遷移：可能 → 戻るボタンを活性状態に変更
+        self.browserBackButton.enabled = YES;
+    }
+    else {
+        // 一つ前のページへの遷移：不可能 → 戻るボタンを非活性状態に変更
+        self.browserBackButton.enabled = NO;
+    }
+    
+    // 進むボタン表示状態更新(webViewでページが進めるかで判定)
+    if ([self.webView canGoForward]) {
+        // 一つ後のページへの遷移：可能 → 進むボタンを活性状態に変更
+        self.browserForwardButton.enabled = YES;
+    }
+    else {
+        // 一つ後のページへの遷移：不可能 → 進むボタンを非活性状態に変更
+        self.browserForwardButton.enabled = NO;
+    }
+    
+    // 中止ボタン表示状態更新(webViewでページが読み込み中かで判別)
+    if ([self.webView isLoading]) {
+        // 読み込み状態：読み込み中 → 中止ボタンを活性状態に変更
+        self.stopButton.enabled = YES;
+    }
+    else {
+        // 読み込み状態：読み込み中でない → 中止ボタンを非活性状態に変更
+        self.stopButton.enabled = NO;
     }
 }
 
